f796fce573981f67d82841aa8ec838f4575cc2d3
Rizky Putra Ednar <49565820+ednar28@users.noreply.github.com>
2023-11-22 12:54:13 +0700

    perbaiki migrasi

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,21 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('id')->increments();
-            $table->string('nama_pengguna',50);
-            $table->string('email',50)->unique();
+            $table->id();
+            $table->string('nama_pengguna', 50);
+            $table->string('email', 50)->unique();
             $table->string('password');
-            $table->string('no_hp_pengguna',15)->nullable();
-            $table->string('no_rek_pengguna',20);
-            $table->string('role',20);
+            $table->string('no_hp_pengguna', 15)->nullable();
+            $table->string('no_rek_pengguna', 20);
+            $table->string('role', 20);
             $table->rememberToken();
             $table->timestamps();
         });
