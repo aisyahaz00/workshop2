a3e2caccddcd8f4dd70e02093d87271ed952bcc4
Rizky Putra Ednar <49565820+ednar28@users.noreply.github.com>
2023-11-22 21:12:11 +0700

    deskripsi ganti string menjadi text

--- database/migrations/2023_09_25_054351_create_produk_table.php
+++ database/migrations/2023_09_25_054351_create_produk_table.php
@@ -13,7 +13,7 @@ return new class () extends Migration {
         Schema::create('produk', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->string('deskripsi');
+            $table->text('deskripsi');
             $table->string('gambar');
             $table->unsignedInteger('harga');
             $table->timestamp('tanggal_dibuat')->nullable();
