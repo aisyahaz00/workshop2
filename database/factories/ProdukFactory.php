bacf2bf14f2fc605c570abb9cb8b05db8c6c5842
Rizky Putra Ednar <49565820+ednar28@users.noreply.github.com>
2023-11-22 21:43:14 +0700

    tambah seeder

--- database/factories/ProdukFactory.php
+++ database/factories/ProdukFactory.php
@@ -17,7 +17,7 @@ class ProdukFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->randomElement(['baju', 'celana', 'jam']) . ' ' . random_int(1, 9999),
+            'nama' => fake()->randomElement(['baju', 'celana', 'jam']) . ' ' . random_int(1, 9999),
             'deskripsi' => fake()->paragraph(),
             'harga' => fake()->randomElement([1_000, 2_000, 5_000]) * random_int(1, 13),
         ];
