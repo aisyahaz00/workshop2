e5f70ec745a2be4abd5c03b69bf9867f31471674
Rizky Putra Ednar <49565820+ednar28@users.noreply.github.com>
2023-12-21 09:06:14 +0700

    buat pemesanan bungkus dengan database transaction

--- app/Http/Controllers/PemesananController.php
+++ app/Http/Controllers/PemesananController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Keranjang;
 use App\Models\Pemesanan;
 use App\Models\PemesananDetail;
+use Illuminate\Support\Facades\DB;
 
 class PemesananController extends Controller
 {
@@ -31,26 +32,32 @@ class PemesananController extends Controller
         return view('pages.shop.pemesanan.detail-pemesanan', ['pemesanan' => $pemesanan]);
     }
 
-    // buat pemesanan
+    /**
+     * Buat pemesanan.
+     */
     public function tambahPemesanan(Keranjang $keranjang)
     {
         // Buat pemesanan baru
-        $pemesanan = new Pemesanan();
-        $pemesanan->user_id = auth()->id();
-        $pemesanan->alamat = auth()->user()->alamat;
-        $pemesanan->tanggal_pemesanan = now();
-        $pemesanan->invoice_number = Pemesanan::buatNomorInvoice(now());
-        $pemesanan->status_pemesanan = 'some_value';
-        $pemesanan->save();
+        DB::transaction(function () use ($keranjang) {
+            $pemesanan = new Pemesanan();
+            $pemesanan->user_id = auth()->user()->id();
+            $pemesanan->alamat = auth()->user()->alamat;
+            $pemesanan->tanggal_pemesanan = now();
+            $pemesanan->invoice_number = Pemesanan::buatNomorInvoice(now());
+            $pemesanan->status_pemesanan = 'some_value';
+            $pemesanan->save();
 
+            // Pindahkan produk dari keranjang ke detail pemesanan
+            $detail = new PemesananDetail();
+            $detail->produk_id = $keranjang->produk_id;
+            $detail->nama_produk = $keranjang->produk->nama;
+            $detail->harga_produk =  $keranjang->produk->harga;
+            $detail->qty = $keranjang->qty;
+            $pemesanan->detail()->save($detail);
 
-        // Pindahkan produk dari keranjang ke detail pemesanan
-        $detail = new PemesananDetail();
-        $detail->produk_id = $keranjang->produk_id;
-        $detail->nama_produk = $keranjang->produk->nama;
-        $detail->harga_produk =  $keranjang->produk->harga;
-        $detail->qty = $keranjang->qty;
-        $pemesanan->detail()->save($detail);
+            // hapus keranjang
+            $keranjang->delete();
+        });
 
         return redirect()->route('shop.pemesanan')->with('success');
     }
